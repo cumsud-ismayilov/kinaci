379ea815b2cfc5f0c0872a8d7c9bb77097379e22
cumsud-ismayilov <c.smayilov1@gmail.com>
2025-08-08 18:07:21 +0400

    navbar and footer partly code add

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,9 +1,110 @@
+import { Link } from "react-router";
+import BackGround from "../assets/footerBackGround.jpg";
+import FooterLogo from "../assets/logoFooter-fc4666aa.png";
+import FaceBook from "../icons/faceBook";
+import Instagram from "../icons/instagram";
+import Twitter from "../icons/twitter";
+import Linkedin from "../icons/linkedin";
+import Youtube from "../icons/youtube";
+import Location from "../icons/location";
+import Envelope from "../icons/envelope";
+import Phone from "../icons/phone";
+
 function Footer() {
   return (
-    <div>
-      footer
-    </div>
-  )
+    <footer className="">
+      <div
+        className="fotter-top h-full bg-cover p-[5rem] bg-center"
+        style={{ backgroundImage: `url(${BackGround})` }}
+      >
+        <div className="grid grid-cols-4 gap-4">
+          <div className="side1 flex flex-col gap-[24px]">
+            <img className="w-[173px]" src={FooterLogo} alt="pageLogo" />
+            <p className="text-white">
+              Kınacı Qrup tikinti və daşınmaz əmlak xidmətləri sektorunda{" "}
+              <b>30 illik təcrübəyə malik şirkətlər qrupudur.</b>
+            </p>
+            <div className="navLeft flex gap-[25px]">
+              <Link>
+                <p>
+                  <FaceBook />
+                </p>
+              </Link>
+              <Link>
+                <p>
+                  <Instagram />
+                </p>
+              </Link>
+              <Link>
+                <p>
+                  <Twitter />
+                </p>
+              </Link>
+              <Link>
+                <p>
+                  <Linkedin />
+                </p>
+              </Link>
+              <Link>
+                <p>
+                  <Youtube />
+                </p>
+              </Link>
+            </div>
+          </div>
+          <div className="side2 text-white flex flex-col gap-[18px]">
+            <p className="h-[47px] text-[22px] font-medium relative before:absolute before:left-0 before:bottom-0 before:h-[4px] before:w-10 before:bg-[#ED6B2C] before:rounded-full before:content-['']">
+              Keçidlər
+            </p>
+            <ul className="list-disc flex flex-col gap-[4px] ">
+              <li className="hover:text-orange-500 transition-all duration-300 ease-in-out">
+                <Link>Kınacı Əmlak haqqında</Link>
+              </li>
+              <li className="hover:text-orange-500 transition-all duration-300 ease-in-out">
+                <Link>Azərbaycanda daşınmaz əmlak</Link>
+              </li>
+              <li className="hover:text-orange-500 transition-all duration-300 ease-in-out">
+                <Link>Bizim xidmətlər</Link>
+              </li>
+              <li className="hover:text-orange-500 transition-all duration-300 ease-in-out">
+                <Link>Geribildirim verin</Link>
+              </li>
+              <li className="hover:text-orange-500 transition-all duration-300 ease-in-out">
+                <Link>Bizimlə əlaqə saxlayın</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="side3 text-white flex flex-col gap-[18px]"> 
+            <p className="h-[47px] text-[22px] font-medium relative before:absolute before:left-0 before:bottom-0 before:h-[4px] before:w-10 before:bg-[#ED6B2C] before:rounded-full before:content-['']">
+             Elektron bülleten
+            </p>
+            <form>
+              <input type="text" placeholder="E-poçt ünvanınızı daxil edin..." className="bg-white p-[16px_10px] rounded-[12px] h-[50px] w-[260px] text-black text-[14px] outline-none" />
+            </form>
+            <button className="p-[12px_32px] bg-[#ED6B2C] text-white rounded-[10px] w-[130px]">Saxla</button>
+            <p className="text-[#ffffff73] text-[13px] italic">Biz sizə lazımsız e-poçt göndərməyəcəyik!</p>
+          </div>
+          <div className="side4  text-white flex flex-col gap-[18px]">
+            <p className="h-[47px] text-[22px] font-medium relative before:absolute before:left-0 before:bottom-0 before:h-[4px] before:w-10 before:bg-[#ED6B2C] before:rounded-full before:content-['']">
+             Kommunikasiya
+            </p>
+            <p className="flex gap-[8px] w-[292px] items-center hover:text-orange-500 transition-all duration-300 ease-in-out cursor-pointer"><Location/>Alanya Mahmutlar Yenili Prospekti No: 11</p>
+            <p className="flex gap-[8px] items-center hover:text-orange-500 transition-all duration-300 ease-in-out cursor-pointer"><Envelope/>info@gmail.com</p>
+            <p className="flex gap-[8px] items-center hover:text-orange-500 transition-all duration-300 ease-in-out cursor-pointer"><Phone/>+994514586806</p>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom p-[1rem_5rem] bg-[#052841]">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="flex gap-[30px]">
+            <p className="text-white text-[15px]"><Link to="/">İstifadə şərtləri</Link></p>
+            <p className="text-white text-[15px]"><Link to="/">Gizlilik</Link></p>
+          </div>
+          <p className="text-white text-[15px] text-center">© Kınacı Gayrimenkul – Tüm hakları saklıdır</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -1,37 +1,186 @@
-import { Link } from "react-router"
-import Envelope from "../icons/envelope"
-import Phone from "../icons/phone"
-import Include from "../icons/include"
-import Heart from "../icons/heart"
-import AzeFlag from "../icons/AzeFlag"
-import DownArrow from "../icons/downArrow"
+import { Link } from "react-router";
+import Envelope from "../icons/envelope";
+import Phone from "../icons/phone";
+import Include from "../icons/include";
+import Heart from "../icons/heart";
+import AzeFlag from "../icons/AzeFlag";
+import DownArrow from "../icons/downArrow";
+import kinaciLogo from "../assets/kinaciLogo.png";
+import FaceBook from "../icons/faceBook";
+import Instagram from "../icons/instagram";
+import Twitter from "../icons/twitter";
+import Linkedin from "../icons/linkedin";
+import Youtube from "../icons/youtube";
+import { useState } from "react";
+import Upchewron from "../icons/upchewron";
 
 function Navbar() {
+  const [activeMenu, setActiveMenu] = useState("/");
+  const [isEmlakOpen, setIsEmlakOpen] = useState(false);
+  const linkClass = (path) =>
+    `cursor-pointer h-full flex items-center hover:text-orange-500 ${
+      activeMenu === path
+        ? "text-orange-500 border-b-2 border-orange-500"
+        : "text-[#052841]"
+    }`;
   return (
-    <header >
-        <div className="navTop bg-blue-50 flex justify-between px-44 py-2.5">
-            <div className="headerLeft flex gap-[10px]">
-                <button className="bg-[#2582C1] text-white  rounded-[5px] p-[6px_8px]"><Link className="flex justify-center items-center  gap-[5px] text-[14px] font-semibold leading-[14px]"><Envelope/> info@kinacigroup.com</Link></button>
-                <button className="bg-[#ED6B2C] text-white rounded-[5px] p-[6px_8px]"><Link className="flex justify-center items-center gap-[5px] text-[14px] font-semibold leading-[14px]"><Phone/> +90(544) 138 07 07</Link></button>
-                <button className="bg-white text-[#ED6B2C] border border-[#ED6B2C] rounded-[5px] p-[6px_8px]"><Link className="flex justify-center items-center gap-[5px] text-[14px] font-semibold leading-[14px]"><Include/> Daxil ol</Link></button>
-            </div>
-            <div className="headerRight flex gap-[10px]">
-                <button className="p-[6px_8px] rounded-[5px] bg-[#E21743]"><Link className="text-white flex gap-[6px] justify-center items-center  text-[14px] font-semibold leading-[14px]">Favoritlərim <Heart/></Link></button>
-                <button className="bg-[#FFFFFF] p-[6px_8px] rounded-[5px]"><Link className="flex justify-center gap-[4px] items-center text-[14px] font-semibold leading-[14px]"><DownArrow/> <AzeFlag/> Aze</Link></button>
-                
-            </div>
+    <header>
+      <div className="navTop bg-blue-50 flex justify-between px-44 py-2.5">
+        <div className="headerLeft flex gap-[10px]">
+          <button className="bg-[#2582C1] text-white  rounded-[5px] p-[6px_8px]">
+            <Link className="flex justify-center items-center  gap-[5px] text-[14px] font-semibold leading-[14px]">
+              <Envelope /> info@kinacigroup.com
+            </Link>
+          </button>
+          <button className="bg-[#ED6B2C] text-white rounded-[5px] p-[6px_8px]">
+            <Link className="flex justify-center items-center gap-[5px] text-[14px] font-semibold leading-[14px]">
+              <Phone /> +90(544) 138 07 07
+            </Link>
+          </button>
+          <button className="bg-white text-[#ED6B2C] border border-[#ED6B2C] rounded-[5px] p-[6px_8px]">
+            <Link className="flex justify-center items-center gap-[5px] text-[14px] font-semibold leading-[14px]">
+              <Include /> Daxil ol
+            </Link>
+          </button>
         </div>
-      {/* <ul>
-        <li><Link to="/">Anasayfa</Link> </li>
-        <li><Link to="/service">Emlak</Link> </li>
-        <li>Şirket hakkında</li>
-        <li>Hizmetlerimiz</li>
-        <li>iletişim</li>
-        <li>Yorumlar</li>
-        <li>Blog</li>
-      </ul> */}
+        <div className="headerRight flex gap-[10px]">
+          <button className="p-[6px_8px] rounded-[5px] bg-[#E21743]">
+            <Link className="text-white flex gap-[6px] justify-center items-center  text-[14px] font-semibold leading-[14px]">
+              Favoritlərim <Heart />
+            </Link>
+          </button>
+          <button className="bg-[#FFFFFF] p-[6px_8px] rounded-[5px]">
+            <Link className="flex justify-center gap-[4px] items-center text-[14px] font-semibold leading-[14px]">
+              <DownArrow /> <AzeFlag /> Aze
+            </Link>
+          </button>
+        </div>
+      </div>
+      <nav className="h-[95px] flex items-center justify-between px-44  shadow-[0_10px_10px_0_rgba(0,0,0,0.03)]">
+        <div className="navRight flex items-center justify-center gap-[40px] h-full">
+          <Link to="/"> 
+            <img
+              className="w-[167px] h-[65px]"
+              src={kinaciLogo}
+              alt="kinaciLogo"
+            />
+          </Link>
+          <ul className="flex gap-[20px] h-full text-[14px] font-semibold leading-[14px]">
+            <li className="h-full relative">
+              <Link
+                to="/"
+                className={linkClass("/")}
+                onClick={() => setActiveMenu("/")}
+              >
+                Ana Səhifə
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/possessions"
+                className={`${linkClass("/possessions")} gap-[4px]`}
+                onClick={() => {
+                  setActiveMenu("/possessions");
+                  setIsEmlakOpen(!isEmlakOpen);
+                }}
+              >
+                Əmlak  {isEmlakOpen ? (
+               <Upchewron />
+            ) : (
+              <DownArrow/>
+            )}
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/about"
+                className={linkClass("/about")}
+                onClick={() => setActiveMenu("/about")}
+              >
+                Şirkət haqqında
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/service"
+                className={`${linkClass("/service")} gap-[4px]`}
+                 onClick={() => {
+                  setActiveMenu("/service");
+                  setIsEmlakOpen(!isEmlakOpen);
+                }}
+              >
+                Xidmətlərimiz {isEmlakOpen ? (
+               <Upchewron />
+            ) : (
+              <DownArrow/>
+            )}
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/contact"
+                className={linkClass("/contact")}
+                onClick={() => setActiveMenu("/contact")}
+              >
+                Əlaqə
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/comments"
+                className={linkClass("/comments")}
+                onClick={() => setActiveMenu("/comments")}
+              >
+                Şərhlər
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/blog"
+                className={linkClass("/blog")}
+                onClick={() => setActiveMenu("/blog")}
+              >
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navLeft flex gap-[25px]">
+          <Link>
+            <p>
+              <FaceBook />
+            </p>
+          </Link>
+          <Link>
+            <p>
+              <Instagram />
+            </p>
+          </Link>
+          <Link>
+            <p>
+              <Twitter />
+            </p>
+          </Link>
+          <Link>
+            <p>
+              <Linkedin />
+            </p>
+          </Link>
+          <Link>
+            <p>
+              <Youtube />
+            </p>
+          </Link>
+        </div>
+      </nav>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
